9bf56d54793942829c3b989ea5b47f8c469ee34b
Lavinia Cotoarba <lavynyutzza@yahoo.com>
2022-04-08 18:52:29 +0300

    [client] remove useless constructor + make ProjectComponent a function

--- client/src/app/components/ProjectComponent.tsx
+++ client/src/app/components/ProjectComponent.tsx
@@ -1,40 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import Collapsible from "./Collapsible";
 
-export default class ProjectComponent extends Component<any, any> {
-    constructor(props){
-        super(props);
-    }
+export default function ProjectComponent(props) {
 
-    render() { 
-        const project = this.props.project;
-        const deadline = this.props.hasData ? new Date(project.deadline).toISOString().substring(0, 10) : "2022-01-01";
+        const project = props.project;
+        const deadline = props.hasData ? new Date(project.deadline).toISOString().substring(0, 10) : "2022-01-01";
         console.log(deadline);
 
         return (
             <>
-                <Collapsible title={this.props.sectionTitle}>
+                <Collapsible title={props.sectionTitle}>
                     <form >	
                         <label htmlFor='name'>Name</label>
-                        <input className="border rounded-full py-2 px-4" type="text" aria-label="Name" id="name" defaultValue={project.name} onChange={this.props.onChangeName}/>
+                        <input className="border rounded-full py-2 px-4" type="text" aria-label="Name" id="name" defaultValue={project.name} onChange={props.onChangeName}/>
                         <br></br>
 
                         <label htmlFor='description'>Description</label>
-                        <input className="border rounded-full py-2 px-4" type="text" aria-label="Description" id="description" defaultValue={project.description} onChange={this.props.onChangeDescription}/>
+                        <input className="border rounded-full py-2 px-4" type="text" aria-label="Description" id="description" defaultValue={project.description} onChange={props.onChangeDescription}/>
                         <br></br>
 
                         <label htmlFor='deadline'>Deadline</label>
-                        <input className="border rounded-full py-2 px-4" type="date" aria-label="Deadline"  id="deadline" defaultValue={deadline} onChange={this.props.onChangeDeadline}/>
+                        <input className="border rounded-full py-2 px-4" type="date" aria-label="Deadline"  id="deadline" defaultValue={deadline} onChange={props.onChangeDeadline}/>
                         <br></br>
 
                         <label htmlFor='clientName'>Client Name</label>
-                        <input className="border rounded-full py-2 px-4" type="text" aria-label="ClientName" id="clientName" defaultValue={project.clientName} onChange={this.props.onChangeClientName}/>
+                        <input className="border rounded-full py-2 px-4" type="text" aria-label="ClientName" id="clientName" defaultValue={project.clientName} onChange={props.onChangeClientName}/>
                         <br></br>
 
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white rounded-full py-2 px-4 ml-2" type="submit" onClick={this.props.onClickAction}>{this.props.clickActionName}</button>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white rounded-full py-2 px-4 ml-2" type="submit" onClick={props.onClickAction}>{props.clickActionName}</button>
                     </form>
                 </Collapsible>
             </>
         );
-    }
 }
