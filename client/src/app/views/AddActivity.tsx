d8ac2f62f5a309920af8c0a61fcfcb22c15d5cdd
Lavinia Cotoarba <lavynyutzza@yahoo.com>
2022-04-11 14:33:13 +0300

    [client] add activity

--- client/src/app/views/AddActivity.tsx
+++ client/src/app/views/AddActivity.tsx
@@ -7,11 +7,14 @@ export default class AddActivity extends Component<any, any> {
         super(props);
         this.saveActivity = this.saveActivity.bind(this);
         this.onChangeName = this.onChangeName.bind(this);
-
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeHours = this.onChangeHours.bind(this);
 
         this.state = {
             name: null,
-            projectId: this.props.projectId
+            projectId: this.props.projectId,
+            description: null,
+            hoursSpent: 0
         };
     }
 
@@ -21,7 +24,6 @@ export default class AddActivity extends Component<any, any> {
                 projectId: this.props.projectId
             });
             console.log(this.state);
-            // this.getAllActivities(this.state.projectId);
         }
     }
 
@@ -45,6 +47,20 @@ export default class AddActivity extends Component<any, any> {
         console.log(this.state);
     }
 
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+        console.log(this.state);
+    }
+
+    onChangeHours(e) {
+        this.setState({
+            hoursSpent: e.target.valueAsNumber
+        });
+        console.log(this.state);
+    }
+
     render() { 
         return (
             <>
@@ -55,11 +71,11 @@ export default class AddActivity extends Component<any, any> {
                     <br></br>
 
                     <label htmlFor='description'>Description</label>
-                    <input className="border rounded-full py-2 px-4" type="text" aria-label="Description" id="description" />
+                    <input className="border rounded-full py-2 px-4" type="text" aria-label="Description" id="description" onChange={this.onChangeDescription}/>
                     <br></br>
 
-                    <label htmlFor='deadline'>Deadline</label>
-                    <input className="border rounded-full py-2 px-4" type="text" aria-label="Hours Spent" id="hoursSpent" />
+                    <label htmlFor='hoursSpent'>Hours Spent</label>
+                    <input className="border rounded-full py-2 px-4" type="number" aria-label="Hours Spent" id="hoursSpent" onChange={this.onChangeHours}/>
                     <br></br>
 
                     <button className="bg-blue-500 hover:bg-blue-700 text-white rounded-full py-2 px-4 ml-2" type="submit" onClick={this.saveActivity}>Add</button>
