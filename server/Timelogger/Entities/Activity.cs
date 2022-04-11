7e87b5afc12e5c07e998fa18291f0d3c298476d1
Lavinia Cotoarba <lavynyutzza@yahoo.com>
2022-04-11 14:33:31 +0300

    [server] change type HoursSpent

--- server/Timelogger/Entities/Activity.cs
+++ server/Timelogger/Entities/Activity.cs
@@ -9,7 +9,7 @@ namespace Timelogger.Entities {
         public int ProjectId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public decimal HoursSpent { get; set; }
+        public int HoursSpent { get; set; }
 
     }
 }
